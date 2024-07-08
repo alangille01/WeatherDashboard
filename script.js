12a1a901e3694cdada08dbfef38000db6379951f
Alexis Langille <langille.alexis@gmail.com>
2024-07-07 23:00:30 -0400

    Create EventListener for Submit Button

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const apiKey = '4f85309e9c88dd09ba2aec8b3161bf78';
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const city = cityInput.value.trim();
+    if (city) {
+        fetchWeather(city);
+        cityInput.value = '';
+    }
+});
